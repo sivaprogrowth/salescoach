848e277579be273b7202b12ad4506c7de3804742
Ubuntu <ubuntu@ip-172-31-12-247.ap-south-1.compute.internal>
2025-09-04 16:58:49 +0000

    Fixing file upload to s3

--- util.py
+++ util.py
@@ -20,7 +20,12 @@ from pdfminer.pdfpage import PDFPage
 load_dotenv()
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-s3 = boto3.client('s3')
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION")
+)
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 AUDIO_DATA_FOLDER = os.getenv("AUDIO_DATA_FOLDER")
 
